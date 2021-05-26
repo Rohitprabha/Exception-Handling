040f16a2ede9f1abeb2c7b08eb0b1ad38d0e94c5
Rohitprabha <67499119+Rohitprabha@users.noreply.github.com>
2021-05-26 18:08:24 +0530

    Add files via upload

---
+++ throw/Throw.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Exception_Handling_5
+{
+    class Throw
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int a = int.Parse(Console.ReadLine());
+                int b = int.Parse(Console.ReadLine());
+                if (b % 2 > 0)
+                {
+                    throw new ApplicationException("Divisor value can't be odd no.");
+                    //ApplicationException ex = new ApplicationException("Divisor value can't be odd no.");
+                    //throw ex;
+                }
+                int result = a / b;
+                Console.WriteLine("result is " + result);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
