8086d3cdcf7c144d5f398edac9e9f4be5e5c89a7
Rohitprabha <67499119+Rohitprabha@users.noreply.github.com>
2021-05-26 18:06:32 +0530

    Add files via upload

---
+++ Finally/Finally.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Exception_Handling_4
+{
+    class Finally
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int a = int.Parse(Console.ReadLine());
+                int b = int.Parse(Console.ReadLine());
+                if (b == 1)
+                {
+                    return;                          //return to outside of the Main even though finally block will execute
+                }
+                int result = a / b;
+                Console.WriteLine("result is " + result);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("finally block executed");
+            }
+            Console.WriteLine("End of the program");
+        }
+    }
+}
