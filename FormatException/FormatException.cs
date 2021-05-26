6dfd515f605f1a045263ec30f6b83f7e1a97edc8
Rohitprabha <67499119+Rohitprabha@users.noreply.github.com>
2021-05-26 18:03:04 +0530

    Add files via upload

---
+++ FormatException/FormatException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Exception_Handling_2
+{
+    class FormatException
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());          // x = 4
+            int y = int.Parse(Console.ReadLine());          // y = y        System.FormatException: 'Input string was not in a correct format.'
+            int result = x + y;
+            Console.WriteLine(result);
+
+        }
+    }
+}
