bc8590c9ef8f68b371f7815baeb31195c8cecb61
Rohitprabha <67499119+Rohitprabha@users.noreply.github.com>
2021-05-26 18:04:38 +0530

    Add files via upload

---
+++ Try_Catch/Try_Catch.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Exception_Handling_3
+{
+    class Try_Catch
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                int a = int.Parse(Console.ReadLine());
+                int b = int.Parse(Console.ReadLine());
+                int result = a / b;
+                Console.WriteLine("result is " + result);
+
+            }
+            catch (DivideByZeroException ex1)
+            {
+                Console.WriteLine(ex1.Message);
+            }
+            catch (FormatException ex2)
+            {
+                Console.WriteLine("Input must be numeric.");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+        }
+    }
+}
