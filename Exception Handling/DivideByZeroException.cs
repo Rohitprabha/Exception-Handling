ff8420663c03e09895ac6ad38c7a3039f1081003
Rohitprabha <67499119+Rohitprabha@users.noreply.github.com>
2021-05-26 18:01:04 +0530

    Add files via upload

---
+++ Exception Handling/DivideByZeroException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Exception_Handling_1
+{
+    class DivideByZeroException
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 0;
+            int result = a / b;                             //System.DivideByZeroException: 'Attempted to divide by zero.'
+            Console.WriteLine("result is " + result);
+        }
+    }
+}
