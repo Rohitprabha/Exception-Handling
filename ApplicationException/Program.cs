82584b9569e69004b5f014eaec85bb1bdab63dd5
Rohitprabha <67499119+Rohitprabha@users.noreply.github.com>
2021-05-26 18:10:22 +0530

    Add files via upload

---
+++ ApplicationException/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Exception_Handling_6
+{
+    public class DivideByOddNoException : ApplicationException
+    {
+        public override string Message
+        {
+            get 
+            {
+                return "Attempted to divide by Odd number";
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            if (b % 2 > 0)
+            {
+                throw new DivideByOddNoException();
+            }
+            int result = a / b;
+            Console.WriteLine("result is " + result);
+        }
+    }
+}
